b7a82e886a910ddeba1282332533fd93b6a3f9a6
TomerBarNatan <110117353+TomerBarNatan@users.noreply.github.com>
2022-08-27 17:52:53 +0300

    Delete ppl1 directory

--- ppl1/src/part3/state.ts
+++
@@ -1,5 +0,0 @@
-export type State<S, A> = (initialState: S) => [S, A];
-
-export const bind:<S,A,B>(state: State<S,A>, f:(x: A)=>State<S,B>) => State<S,B> = function<S,A,B>(state: State<S,A>, f:((x: A)=>State<S,B>)){
-    return ((s:S)=> { const [s1,a]: [S,A] = state(s); return f(a)(s1)})
-};
